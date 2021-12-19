70e94e67c8123cb5d095e2615f1a1a19d6dac47d
HadyHM <93319621+HadyHM@users.noreply.github.com>
2021-12-19 21:15:43 +0200

    Add files via upload

---
+++ Project/JS/javascript.js
@@ -0,0 +1,19 @@
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  var myVar;
+
+function myFunction1() {
+  myVar = setTimeout(showPage, 1000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
